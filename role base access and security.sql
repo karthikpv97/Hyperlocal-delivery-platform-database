69526d08ecbea079fa441df8b7a2199e28cc31d9
karthikpv97 <karthikpv679@gmail.com>
2025-05-14 22:23:36 +0530

    role base access and security.sql

--- role base access and security.sql
+++ role base access and security.sql
@@ -11,12 +11,12 @@ GRANT 'AdminRole' TO 'admin_user'@'localhost';
 GRANT 'VendorRole' TO 'vendor_user'@'localhost';
 
 -- 4. Grant privileges to roles
-GRANT SELECT, INSERT, UPDATE, DELETE ON zeptoDB.Users TO 'AdminRole';
-GRANT SELECT, UPDATE ON zeptoDB.Products TO 'AdminRole';
-GRANT SELECT, UPDATE ON zeptoDB.Inventory TO 'VendorRole';
+GRANT SELECT, INSERT, UPDATE, DELETE ON Users TO 'AdminRole';
+GRANT SELECT, UPDATE ON Products TO 'AdminRole';
+GRANT SELECT, UPDATE ON Inventory TO 'VendorRole';
 
 -- 5. Revoke specific privilege
-REVOKE INSERT ON zeptoDB.Users FROM 'AdminRole';
+REVOKE INSERT ON Users FROM 'AdminRole';
 
 -- 6. Set role (optional within session if required)
 SET ROLE 'AdminRole';
